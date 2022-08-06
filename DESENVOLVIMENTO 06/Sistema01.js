8b22279c34d2504cb3d42b0f75ef2487644cdf06
MarioYama7 <mariohenrique.777@gmail.com>
2022-08-06 15:18:35 -0300

    Desenvolvimento 06

--- DESENVOLVIMENTO 06/Sistema01.js
+++ DESENVOLVIMENTO 06/Sistema01.js
@@ -7,16 +7,9 @@ console.log ('Sistema para notas de alunos')
 const notaA = Number(input.question('Digite sua primeira nota: '))
 const notaB = Number(input.question('Digite sua segunda nota: '))
 const notaC = Number(input.question('Digite sua terceira nota: '))
-let resultadoFinal = '' /*essa variável tem que ser em let, pois, após o if vai ser incrementado um valor a ela. Se colocar const vai dar erro*/
 const media = ((notaA + notaB + notaC) / 3)
 
-if (media >=7){
-    resultadoFinal = 'Você foi Aprovado'
-}
-
-else {
-    resultadoFinal ='Você foi Reprovado'
-}
+let resultadoFinal = (media >= 7 ? 'Aluno Approvado' : 'Aluno Reprovado') /*Operador ternário*/
 
 console.clear() /*limpar a tela antes de mostrar o resultado*/
 
