d8c4ea7d7c53f7110b23672bf71201cc3d0e7324
MarioYama7 <mariohenrique.777@gmail.com>
2022-08-11 16:13:12 -0300

    Desenvolvimento 11

---
+++ DESENVOLVIMENTO 11/Des11.js
@@ -0,0 +1,31 @@
+/*Construção de objeto*/
+
+const bolo = {
+    massa: "chocolate",
+    recheio: "ninho",
+    pesoEmKg: 1
+}
+
+/*Função para o objeto*/
+const propriedades = () => {console.log("A massa do bolo é de: " + 
+    bolo.massa + " o recheio é de: " + 
+    bolo.recheio + " e o peso é de: " +
+    bolo.pesoEmKg)
+}
+
+/*Construção do Array*/
+const arr1 = () => {
+    return ["Mário", "Roberto", "Ana"]
+}
+
+/*Função para o Array*/
+
+const listaArray = () => {
+    console.log("O nome dos filhos são: " + arr1()[0] + ", " + arr1()[1] + " e " + arr1()[2] + ".")
+}
+
+/*Exibição das funções, pois as duas contém os arrays e precisam ser "chamadas" por fora*/
+
+propriedades();
+listaArray();
+
